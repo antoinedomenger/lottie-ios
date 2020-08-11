e0d0a756eb3762b79e5ae8e46d2b1a36561040c6
Antoine Domenger <antoine@domenger.fr>
2020-08-11 15:40:16 +0200

    Add @objc attribute

--- lottie-swift/src/Public/Animation/AnimationViewInitializers.swift
+++ lottie-swift/src/Public/Animation/AnimationViewInitializers.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public extension AnimationView {
+@objc public extension AnimationView {
 
     @objc convenience init(name: String,
                            bundle: Bundle = Bundle.main) {
@@ -26,10 +26,10 @@ public extension AnimationView {
    - Parameter imageProvider: An image provider for the animation's image data.
    If none is supplied Lottie will search in the supplied bundle for images.
    */
-  convenience init(name: String,
-                          bundle: Bundle = Bundle.main,
-                          imageProvider: AnimationImageProvider? = nil,
-                          animationCache: AnimationCacheProvider? = LRUAnimationCache.sharedCache) {
+  @nonobjc convenience init(name: String,
+                            bundle: Bundle = Bundle.main,
+                            imageProvider: AnimationImageProvider? = nil,
+                            animationCache: AnimationCacheProvider? = LRUAnimationCache.sharedCache) {
     let animation = Animation.named(name, bundle: bundle, subdirectory: nil, animationCache: animationCache)
     let provider = imageProvider ?? BundleImageProvider(bundle: bundle, searchPath: nil)
     self.init(animation: animation, imageProvider: provider)
@@ -42,9 +42,9 @@ public extension AnimationView {
    - Parameter imageProvider: An image provider for the animation's image data.
    If none is supplied Lottie will search in the supplied filepath for images.
    */
-  convenience init(filePath: String,
-                          imageProvider: AnimationImageProvider? = nil,
-                          animationCache: AnimationCacheProvider? = LRUAnimationCache.sharedCache) {
+  @nonobjc convenience init(filePath: String,
+                            imageProvider: AnimationImageProvider? = nil,
+                            animationCache: AnimationCacheProvider? = LRUAnimationCache.sharedCache) {
     let animation = Animation.filepath(filePath, animationCache: animationCache)
     let provider = imageProvider ?? FilepathImageProvider(filepath: URL(fileURLWithPath: filePath).deletingLastPathComponent().path)
     self.init(animation: animation, imageProvider: provider)
@@ -58,10 +58,10 @@ public extension AnimationView {
    If none is supplied Lottie will search in the main bundle for images.
    - Parameter closure: A closure to be called when the animation has loaded.
    */
-  convenience init(url: URL,
-                          imageProvider: AnimationImageProvider? = nil,
-                          closure: @escaping AnimationView.DownloadClosure,
-                          animationCache: AnimationCacheProvider? = LRUAnimationCache.sharedCache) {
+  @nonobjc convenience init(url: URL,
+                            imageProvider: AnimationImageProvider? = nil,
+                            closure: @escaping AnimationView.DownloadClosure,
+                            animationCache: AnimationCacheProvider? = LRUAnimationCache.sharedCache) {
     
     if let animationCache = animationCache, let animation = animationCache.animation(forKey: url.absoluteString) {
       self.init(animation: animation, imageProvider: imageProvider)
